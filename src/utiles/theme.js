8e77f3d80e9e51f9b4582ad8ea19c9db7a8a269b
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-03-03 16:19:08 +0200

    add template

---
+++ src/utiles/theme.js
@@ -0,0 +1,27 @@
+export const theme = {
+    colors: {
+        page:'#e4d3f2',
+        primary: '#b8c0c2',
+        secondary: '#9342f5',
+        accent:'#e1a5f2',
+    },
+    text: {
+        primary: '#000000',
+        secondary:'#756b6b',
+    },
+    fontSizes: {
+        s: '12px',
+        m: '18px',
+        l: '24px',
+    },
+    radii: {
+        normal: '4px',
+        circle: '50%',
+    },
+    shadow: '10px 5px 5px #9342f5',    
+    space: [0, 2, 4, 8, 16, 32],
+    status: {
+        online: '#12992d',
+        offline:'#bd132a',
+    },    
+}
